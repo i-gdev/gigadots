abf94aa2a4c1ba247a451093d31df6201b77e390
GigaDev <164503373+i-gdev@users.noreply.github.com>
2024-04-10 07:39:58 +0300

    Changed setmouse.sh from using id's to device name
    xinput will change device id's, but a string containing the device name can also be used in place of id.

--- .local/bin/setmouse.sh
+++ .local/bin/setmouse.sh
@@ -1,4 +1,4 @@
 # Sets mouse acceleration off, and adjusts sensitivity
 
-xinput set-prop 10 "Coordinate Transformation Matrix" 1.0 0.0 0.0 0.0 1.0 0.0 0 0 1
-xinput set-prop 10 "libinput Accel Profile Enabled" 0 1
+xinput set-prop "Logitech USB Receiver Mouse" "Coordinate Transformation Matrix" 1.0 0.0 0.0 0.0 1.0 0.0 0 0 1
+xinput set-prop "Logitech USB Receiver Mouse" "libinput Accel Profile Enabled" 0 1
